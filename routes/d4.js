7fb244b0a5b80ee982d5937dc8c9def2ba118887
nemoDemon <235113@stud.umk.pl>
2018-01-03 20:45:57 +0100

    second

--- routes/d4.js
+++ routes/d4.js
@@ -2,12 +2,12 @@ const fetch = require("node-fetch");
 
 var methods =
 {
-	 getLocation: async function(addressUrl)
+	 getLocation: function(addressUrl)
 	{
 		try
 		{
-		  const response = await fetch(addressUrl);
-		  const json = await response.json();
+		  const response = fetch(addressUrl);
+		  const json = response.json();
 
 		  var address = json.results[0].formatted_address;
 		  var latitude = json.results[0].geometry.location.lat;
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const addressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var webAddress = d4.modules.getLocation(addressUrl).toString();
+  var webAddress = d4.modules.getLocation(addressUrl);
   res.render('index', { title: webAddress });
 });
 
