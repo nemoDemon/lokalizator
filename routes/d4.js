36fa8312bbd5eebaf15d2f7c246bd7061ee44371
nemoDemon <235113@stud.umk.pl>
2018-01-03 21:24:51 +0100

    second

--- routes/d4.js
+++ routes/d4.js
@@ -6,24 +6,24 @@ var methods =
 	{
 		try
 		{
-		  const response = fetch(addressUrl);
-		  const json = response.json();
+			const response = fetch(addressUrl);
+			const json = response.json();
 
-		  var address = json.results[0].formatted_address;
-		  var latitude = json.results[0].geometry.location.lat;
-		  var longtitude = json.results[0].geometry.location.lng;
-	  
-		  console.log("Address: " + address);
-		  console.log("Latitude: " + latitude);
-		  console.log("Longtitude: " + longtitude);
+			var address = json.results[0].formatted_address;
+			var latitude = json.results[0].geometry.location.lat;
+			var longtitude = json.results[0].geometry.location.lng;
 
-		  var returnedString = "Address: " + address;
-		  return returnedString.toString();
+			console.log("Address: " + address);
+			console.log("Latitude: " + latitude);
+			console.log("Longtitude: " + longtitude);
+
+			var returnedString = "Address: " + address;
+			return returnedString.toString();
 		}
 		catch(error)
 		{
 			console.log(error);
-return "Error";
+			return "Error: " + error;
 		}
 
 	}
