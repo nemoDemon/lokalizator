33e72441330a901518cced7a0d46b3111edbcf71
nemoDemon <235113@stud.umk.pl>
2018-01-03 19:07:31 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var d4 = require('d4.js');
+var d4 = require('./d4.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
