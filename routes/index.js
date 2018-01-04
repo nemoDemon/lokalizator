27197ee5b1aa50da366ef131e519f5e13479a669
nemoDemon <235113@stud.umk.pl>
2018-01-04 13:34:13 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ const addressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-	var zmienna =await d4.modules.getLocation(addressUrl);
-//d4.modules.getLocation(addressUrl).then(function(response)
-//{res.render('index', response)}
-//);
-  res.render('index', { title: zmienna });
+	var zmienna = await d4.modules.getLocation(addressUrl);
+	res.render('index', { title: zmienna });
+});
+
+router.post('/', async function (req, res, next) {
+	var add = "https://maps.googleapis.com/maps/api/geocode/json?address="+req;
+  	var zmienna = await d4.modules.getLocation(add);
+	res.render('index', { title: zmienna });
 });
 
 module.exports = router;
