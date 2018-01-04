5e8fd68e3b42890b4f17d3e88afc6903174da9c3
nemoDemon <235113@stud.umk.pl>
2018-01-04 18:35:22 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var bodyParser = require("body-parser");
-var app = express();
 var router = express.Router();
 
 var d4 = require('./d4.js');
 const addressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+"Torun";
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', async function(req, res) {
@@ -15,12 +10,10 @@ router.get('/', async function(req, res) {
 	res.render('index', { title: zmienna });
 });
 
-router.post('/', async function (req, res) {
-	var postVar = req.body.town;
-	var add = "https://maps.googleapis.com/maps/api/geocode/json?address="+town;
-console.log(add);
-  	var zmienna = await d4.modules.getLocation(add);
-	res.render('index', { title: zmienna });
+router.post('/', async function(req, res) {
+	var town = req.body.town;
+	res.render('index', { title: town });
 });
+
 
 module.exports = router;
