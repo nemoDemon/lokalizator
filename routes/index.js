fff409d50e7f4eb065523e52079be88a90c8187c
nemoDemon <235113@stud.umk.pl>
2018-01-04 19:20:08 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', async function(req, res) {
 router.post('/', async function(req, res) {
 	var town = await req.body.town;
 	var address = "https://maps.googleapis.com/maps/api/geocode/json?address="+town;
-	var zmienna = await d4.modules.getLocation(addressUrl);
+	var zmienna = town;//await d4.modules.getLocation(addressUrl);
 	res.render('index', { title: zmienna });
 });
 
