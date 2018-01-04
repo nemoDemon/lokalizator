0c21f9e6dc3f25fc7f96181fbbcb00d4fd344178
nemoDemon <235113@stud.umk.pl>
2018-01-04 23:37:03 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var d4 = require('./d4.js');
 
 /* GET home page. */
 router.get('/', async function(req, res) {
-	var answer = "Hello";
-	res.render('index', { title: answer });
+	var title = "Lokalizator";
+	var text = "Please put address you are searching for in request text box"
+	res.render('index', { title: title, text: text });
 });
 
 router.post('/', async function(req, res) {
@@ -19,16 +20,18 @@ router.post('/', async function(req, res) {
 	var latitude = adressArray[1];
 	var longtitude = adressArray[2];
 
-	var elevationsUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude;//+"&key=YOUR_API_KEY";
+	var elevationsUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude;
 	var elevation = await d4.modules.getElevation(elevationsUrl);
 
 	var timezoneOffset = new Date().getTimezoneOffset();
-	var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset;//&key=YOUR_API_KEY
+	var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset;
 	var dateAndTime = await d4.modules.getDateAndTime(dateAndTimeUrl);
 
-	var answer = "<br />Address: " + address + "<br />Latitude: " + latitude + "<br />Longtitude: " + longtitude + " Date and time: " + dateAndTime;
+	//var answer = "Address: " + address + " Latitude: " + longtitude + " Longtitude: " + longtitude + " Date and time: " + dateAndTime;
 
-	res.render('index', { title: answer });
+	var title = "Lokalizator";
+	var text = "Found details are presented below..."
+	res.render('index', { title: title, text: text, address: address, latitude: latitude, longtitude: longtitude, dateAndTime: dateAndTime });
 });
 
 
