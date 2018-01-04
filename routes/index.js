8a75071e532d525b404171d08addb387151e1993
nemoDemon <235113@stud.umk.pl>
2018-01-04 17:12:38 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ var d4 = require('./d4.js');
 const addressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+"Torun";
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function(req, res) {
 	var zmienna = await d4.modules.getLocation(addressUrl);
 	res.render('index', { title: zmienna });
 });
 
-router.post('/', async function (req, res, next) {
+router.post('/', async function (req, res) {
 	console.log("req: " + req);
-	var add = await "https://maps.googleapis.com/maps/api/geocode/json?address="+req;
+	var add = "https://maps.googleapis.com/maps/api/geocode/json?address="+req;
   	var zmienna = await d4.modules.getLocation(add);
 	res.render('index', { title: zmienna });
 });
