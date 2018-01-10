dbc96a1234348deaf2c236862dc87467719bdcf6
nemoDemon <235113@stud.umk.pl>
2018-01-10 10:53:42 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,22 @@ router.get('/', async function(req, res) {
 	res.render('index', { title: title, text: text });
 });
 
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
 	var addressPosted=  req.body.town;
 	var adressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+addressPosted;
-	var adressReturned = d4.modules.getLocation(adressUrl);
-	var adressArray = adressReturned.toString().split(";");
+	var adressReturned = await d4.modules.getLocation(adressUrl);
+	var adressArray = adressReturned.split(";");
 
 	var address = adressArray[0];
 	var latitude = adressArray[1];
 	var longtitude = adressArray[2];
 
 	var elevationUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude;
-	var elevation = d4.modules.getElevation(elevationUrl);
+	var elevation = await d4.modules.getElevation(elevationUrl);
 
 	var timezoneOffset = new Date().getTimezoneOffset();
 	var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset;
-	var dateAndTime = d4.modules.getDateAndTime(dateAndTimeUrl);
+	var dateAndTime = await d4.modules.getDateAndTime(dateAndTimeUrl);
 
 	var title = "Lokalizator";
 	var text = "Found details are presented below..."
