ceb869bb6bc18cfe735d7aa89321b6a511f38658
nemoDemon <235113@stud.umk.pl>
2018-01-10 12:41:34 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -12,26 +12,36 @@ router.get('/', async function(req, res) {
 
 router.post('/', async function(req, res) {
 	var addressPosted =  req.body.town;
-	var adressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+addressPosted+"&key=AIzaSyC_R5Bj6peTehs6YEzuXCDPpqa-VndhKaI";
-	var adressReturned = await d4.modules.getLocation(adressUrl);
-	var adressArray = adressReturned.split(";");
-
-	var address = adressArray[0];
-	var latitude = adressArray[1];
-	var longtitude = adressArray[2];
-
-	var elevationUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude+"&key=AIzaSyAOCM4z1CH2j0LldnBXPXh91fKlx8ZTMBk";
-	var elevation = await d4.modules.getElevation(elevationUrl);
-
-	var timezoneOffset = new Date().getTimezoneOffset();
-	var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset+"&key=AIzaSyAE2Fap3dFbhYgs7OfhMsCKz97u1fI7Lcs";
-	var dateAndTime = await d4.modules.getDateAndTime(dateAndTimeUrl);
-
+	if(addressPosted == '')
+	{
 	var title = "Lokalizator";
-	var text = "Found details are presented below..."
+	var text = "Address cannot be empty..."
 
 	res.render('index', { title: title, text: text, address: address, latitude: latitude, longtitude: longtitude, elevation: elevation, dateAndTime: dateAndTime });
 
+	}
+	else
+	{
+		var adressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+addressPosted+"&key=AIzaSyC_R5Bj6peTehs6YEzuXCDPpqa-VndhKaI";
+		var adressReturned = await d4.modules.getLocation(adressUrl);
+		var adressArray = adressReturned.split(";");
+
+		var address = adressArray[0];
+		var latitude = adressArray[1];
+		var longtitude = adressArray[2];
+
+		var elevationUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude+"&key=AIzaSyAOCM4z1CH2j0LldnBXPXh91fKlx8ZTMBk";
+		var elevation = await d4.modules.getElevation(elevationUrl);
+
+		var timezoneOffset = new Date().getTimezoneOffset();
+		var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset+"&key=AIzaSyAE2Fap3dFbhYgs7OfhMsCKz97u1fI7Lcs";
+		var dateAndTime = await d4.modules.getDateAndTime(dateAndTimeUrl);
+
+		var title = "Lokalizator";
+		var text = "Found details are presented below..."
+
+		res.render('index', { title: title, text: text, address: address, latitude: latitude, longtitude: longtitude, elevation: elevation, dateAndTime: dateAndTime });
+	}
 });
 
 
