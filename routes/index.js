b3f6e6e4e5d5247a0ca3413947598875be3e2cb2
nemoDemon <235113@stud.umk.pl>
2018-01-10 10:47:15 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', async function(req, res) {
 router.post('/', async function(req, res) {
 	var addressPosted=  req.body.town;
 	var adressUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+addressPosted;
-	var adressReturned = await d4.modules.getLocation(adressUrl);
+	var adressReturned = d4.modules.getLocation(adressUrl);
 	var adressArray = adressReturned.split(";");
 
 	var address = adressArray[0];
@@ -21,11 +21,11 @@ router.post('/', async function(req, res) {
 	var longtitude = adressArray[2];
 
 	var elevationUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latitude+","+longtitude;
-	var elevation = await d4.modules.getElevation(elevationUrl);
+	var elevation = d4.modules.getElevation(elevationUrl);
 
 	var timezoneOffset = new Date().getTimezoneOffset();
 	var dateAndTimeUrl = "https://maps.googleapis.com/maps/api/timezone/json?location="+latitude+","+longtitude+"&timestamp="+timezoneOffset;
-	var dateAndTime = await d4.modules.getDateAndTime(dateAndTimeUrl);
+	var dateAndTime = d4.modules.getDateAndTime(dateAndTimeUrl);
 
 	var title = "Lokalizator";
 	var text = "Found details are presented below..."
